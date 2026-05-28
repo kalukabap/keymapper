3fe3e3081cd339a237585a77ae5b7ec993b34590
kalukabap <kalukabap@users.noreply.github.com>
2026-05-28 12:19:26 +0530

    Add repositories to buildscript for kotlin-reflect

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-reflect:2.3.20")
     }
